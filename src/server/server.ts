0491e6f7db110c831c4bd1ffbd6f6f8faeb14d73
fpuricelli@gmail.com <fpuricelli@gmail.com>
2022-05-18 17:53:56 -0300

    error port

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,7 +4,7 @@ import http from 'http'
 import { Server, Socket } from 'socket.io'
 import { nextTick } from 'process'
 
-const port: process.env.PORT || 3000
+const port: process.env.PORT
 
 class App {
     private server: http.Server
